359ba930f2d686ad13ee354a31abc384a3065bf2
Golub Alexandra <golub.av@phystech.edu>
2022-05-20 17:20:24 +0300

    part of detail screen

--- app/src/main/java/com/example/sonder_restaurant/features/detailsScreen/presentation/fragments/DetailFragment.kt
+++ app/src/main/java/com/example/sonder_restaurant/features/detailsScreen/presentation/fragments/DetailFragment.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.example.sonder_restaurant.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 
 class detailFragment : Fragment() {
+    private lateinit var backButton: Button
+    private lateinit var favouritesButton: Button
+    private lateinit var dishImage: CircleImageView
+    private lateinit var dishName: TextView
+    private lateinit var dishPrice: TextView
+    private lateinit var dishComposition: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView(view)
+    }
+
+    private fun initView(view: View) {
+        backButton = view.findViewById(R.id.btn_back_detail_fragment)
+        favouritesButton = view.findViewById(R.id.btn_likes_dish)
+        dishImage = view.findViewById(R.id.iv_picture_of_dish_detail)
+        dishName = view.findViewById(R.id.tv_name_of_dish_detail)
+        dishPrice = view.findViewById(R.id.tv_price_detail)
+        dishComposition = view.findViewById(R.id.composition_dish)
     }
 
 }
